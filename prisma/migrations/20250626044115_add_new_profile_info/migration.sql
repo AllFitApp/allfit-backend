c6697fc27a1e54baef950c3dcaed895fdcf39227
Dargouls <gah_500@hotmail.com>
2025-06-26 02:13:10 -0300

    feat: add more infos

---
+++ prisma/migrations/20250626044115_add_new_profile_info/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "user_infos" ADD COLUMN     "formation" TEXT,
+ADD COLUMN     "onlineService" TEXT,
+ADD COLUMN     "presencialService" TEXT,
+ADD COLUMN     "specialty" TEXT;
