84e346e02ae5603f394cf223bec16e2345729e71
Dargouls <gah_500@hotmail.com>
2025-08-28 22:06:06 -0300

    fix: adicionado suporte para conexões com pooler

--- src/adapters/PrismaClientAdapter.ts
+++ src/adapters/PrismaClientAdapter.ts
@@ -1,53 +1,41 @@
 import { PrismaClient } from "@prisma/client";
 import { IDatabaseClient } from "../interfaces/IDatabaseClient";
+import { prisma } from './prismaClient';
 
 type PrismaModels = {
-  [K in keyof PrismaClient]: PrismaClient[K] extends { findUnique: any }
-    ? PrismaClient[K]
-    : never;
+	[K in keyof PrismaClient]: PrismaClient[K] extends { findUnique: any; }
+	? PrismaClient[K]
+	: never;
 };
 
 export class PrismaClientAdapter implements IDatabaseClient {
-  private prisma: PrismaClient;
+	private prisma = prisma; // usa instância global
 
-  constructor() {
-    this.prisma = new PrismaClient();
-  }
+	private getModel<T>(model: keyof PrismaModels): any {
+		const repository = this.prisma[model];
+		if (!repository) {
+			throw new Error(`Model ${String(model)} not found in PrismaClient.`);
+		}
+		return repository;
+	}
 
-  private getModel<T>(model: keyof PrismaModels): any {
-    const repository = this.prisma[model];
-    if (!repository) {
-      throw new Error(`Model ${String(model)} not found in PrismaClient.`);
-    }
-    return repository;
-  }
+	async findOne<T>(model: string, where: Record<string, any>): Promise<T | null> {
+		return this.getModel(model as keyof PrismaModels).findUnique({ where }) as Promise<T | null>;
+	}
 
-  async findOne<T>(model: string, where: Record<string, any>): Promise<T | null> {
-    const repository = this.getModel(model as keyof PrismaModels);
-    return repository.findUnique({ where }) as Promise<T | null>;
-  }
+	async findMany<T>(model: string, where: Record<string, any> = {}): Promise<T[]> {
+		return this.getModel(model as keyof PrismaModels).findMany({ where }) as Promise<T[]>;
+	}
 
-  async findMany<T>(model: string, where: Record<string, any> = {}): Promise<T[]> {
-    const repository = this.getModel(model as keyof PrismaModels);
-    return repository.findMany({ where }) as Promise<T[]>;
-  }
+	async create<T>(model: string, data: Record<string, any>): Promise<T> {
+		return this.getModel(model as keyof PrismaModels).create({ data }) as Promise<T>;
+	}
 
-  async create<T>(model: string, data: Record<string, any>): Promise<T> {
-    const repository = this.getModel(model as keyof PrismaModels);
-    return repository.create({ data }) as Promise<T>;
-  }
+	async update<T>(model: string, where: Record<string, any>, data: Record<string, any>): Promise<T> {
+		return this.getModel(model as keyof PrismaModels).update({ where, data }) as Promise<T>;
+	}
 
-  async update<T>(
-    model: string,
-    where: Record<string, any>,
-    data: Record<string, any>
-  ): Promise<T> {
-    const repository = this.getModel(model as keyof PrismaModels);
-    return repository.update({ where, data }) as Promise<T>;
-  }
-
-  async delete<T>(model: string, where: Record<string, any>): Promise<void> {
-    const repository = this.getModel(model as keyof PrismaModels);
-    await repository.delete({ where });
-  }
+	async delete<T>(model: string, where: Record<string, any>): Promise<void> {
+		await this.getModel(model as keyof PrismaModels).delete({ where });
+	}
 }
